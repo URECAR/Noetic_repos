c13167650dbe4b6b777172a793a0644b0624b434
URECAR <baejh08@gmail.com>
2025-04-04 16:24:25 +0900

    backup

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+from catkin_pkg.python_setup import generate_distutils_setup
+
+d = generate_distutils_setup(
+    packages=['ur10e_2f_140_control'],
+    package_dir={'': 'scripts'}
+)
+
+setup(**d)
